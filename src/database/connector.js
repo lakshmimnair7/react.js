58ac0b28362d4782fd9deb322ce565ce671ca0d3
ravishanker M <ravishankerm9@gmail.com>
2020-02-17 15:00:03 +0530

    change

--- src/database/connector.js
+++ src/database/connector.js
@@ -1,12 +1,22 @@
 const sequelize = require('sequelize');
 const config = require('../config/config');
 
-const db = new sequelize{
+const db = new sequelize(
     config.db.database,
     config.db.user,
     config.db.password,
     {
         host:config.db.host,
+        dialect : "mysql",
+        operatorAliases: false,
+        logging: config.db.logging,
+        pool:{
+                max:10,
+                min:0,
+                acquire: 30000,
+                idle: 10000
+        }
     }
 
-}
+
+);
