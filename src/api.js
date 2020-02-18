50ed0851669d9f51d2c35062bc2e892c3af7b332
ravishanker M <ravishankerm9@gmail.com>
2020-02-18 15:53:11 +0530

    final

--- src/api.js
+++ src/api.js
@@ -1,27 +1,34 @@
 const express = require('express')
 const Product = require('./database/models/Product')
 const router = express.Router();
-
+const Manufacture= require('./database/models/Manufacturer')
 
 
 router.get("/",(req,res) => {
     Product.findAll({
-        include:[{model:manufacture}]
+        include:[{model:Manufacture}]
     }).then(res1 => {
          res.json(res1);
     })
 })
 
-router.post('/product',(req,res) => {
+router.post('/',(req,res) => {
     if(req.body !== null){
         console.log(req.body)
         let newpdt={
             name:req.body.name,
             price:req.body.price
         };
+        let newManu = req.body.Manufacture;
         Product.create(newpdt).then(pdt => {
-            console.log('saved')
+            Manufacture.findOne({where:{id:newManu}}).then(resManu => {
+                pdt.setManufactures(resManu)
+                console.log('saved')
+            })
+            
         })
+        
+       
 
     }
      res.json({"status":"executed"});
